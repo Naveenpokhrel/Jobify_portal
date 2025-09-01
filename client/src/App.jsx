1855c890dd8bad31e7cfd82a1e968c3a487f54dd
Naveen <naveenpokhrel99@gmail.com>
2025-09-01 22:43:48 +0545

    set up completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import { ApplyJob } from "./pages/ApplyJob";
+import Applications from "./pages/Applications";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
-      <p>Hello World</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/applications" element={<Applications />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App
