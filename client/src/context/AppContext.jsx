0cf9431ae759ea1e20cefa00e117846a01e07c78
Naveen <naveenpokhrel99@gmail.com>
2025-09-01 23:59:43 +0545

    updated

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,14 +1,22 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-    const value = {
+  const [searchFilter, setSearchFilter] = useState({
+    tittle:'',
+    location:''
+  });
 
-    }
+  const [isSearched, setIsSearched] = useState(false)
 
-    return (<AppContext.Provider value={value}>
-        {props.children}
-    </AppContext.Provider>)
-}
+  const value = {
+    searchFilter,
+    setSearchFilter,
+    isSearched,setIsSearched,
+  };
 
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
