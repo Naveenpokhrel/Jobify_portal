66ee1320c349156c116f44620dcde4210406515a
Naveen <naveenpokhrel99@gmail.com>
2025-09-03 00:59:33 +0545

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { Link } from "react-router-dom";
-import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
-const Navbar = () => {
+import React, { useContext } from "react";
+import { assets } from "../assets/assets";
+import { Link, useNavigate } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { AppContext } from "../context/AppContext";
 
-  const {openSignIn} = useClerk()
+const Navbar = () => {
+  const { openSignIn } = useClerk();
   const { user } = useUser();
+
+  const navigate = useNavigate();
+
+  const { setShowRecruiterLogin } = useContext(AppContext); // ✅ Fixed name
 
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
-        <img src={assets.logo} alt="" />
-        {
-          user 
-          ? <div className='flex items-center gap-3'>
-            <Link to={'/applications'}>Applied Jobs</Link>
+        <img
+          onClick={() => navigate("/")}
+          className="cursor-pointer"
+          src={assets.logo}
+          alt="logo"
+        />
+        {user ? (
+          <div className="flex items-center gap-3">
+            <Link to={"/applications"}>Applied Jobs</Link>
             <p>|</p>
-            <p className='max-sm:hidden'>Hi, {user.firstName+" "+user.lastName}</p>
+            <p className="max-sm:hidden">
+              Hi, {user.firstName + " " + user.lastName}
+            </p>
             <UserButton />
-
           </div>
-        :  <div className="flex gap-4 max-sm:text-xs">
-            <button className="text-gray-600">Recruiter Login</button>
+        ) : (
+          <div className="flex gap-4 max-sm:text-xs">
+            <button
+             onClick={e=> setShowRecruiterLogin(true)} 
+            >
+              Recruiter Login
+            </button>
             <button
               onClick={() => openSignIn()}
               className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
@@ -29,10 +44,10 @@ const Navbar = () => {
               Login
             </button>
           </div>
-        }
+        )}
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
