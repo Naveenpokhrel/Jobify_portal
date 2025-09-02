e2d3517374b60289cca5259753f824d580cd7b28
Naveen <naveenpokhrel99@gmail.com>
2025-09-02 20:33:30 +0545

    adding the apply job and home

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 export const ApplyJob = () => {
-  return (
-    <div>ApplyJob</div>
-  )
-}
+  const { id } = useParams();
+
+  const [JobData, setJobData] = useState(null);
+
+  const { jobs } = useContext(AppContext);
+
+  useEffect(() => {
+    const data = jobs.filter((job) => job._id === id);
+    if (data.length !== 0) {
+      setJobData(data[0]);
+      console.log(data[0]);
+    }
+  }, [id, jobs]);
+  return <div> hello world</div>;
+};
