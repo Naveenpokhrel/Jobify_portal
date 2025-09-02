6bb78bc640423d264f3cff8a670a73b1c9b69ad7
Naveen <naveenpokhrel99@gmail.com>
2025-09-02 20:32:43 +0545

    updated the code of app

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,19 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { jobsData } from "../assets/assets";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
   const [searchFilter, setSearchFilter] = useState({
-    tittle:'',
-    location:''
+    tittle: "",
+    location: "",
   });
 
-  const [isSearched, setIsSearched] = useState(false)
+  const [isSearched, setIsSearched] = useState(false);
+
+  const [jobs, setJobs] = useState([]);
+
+  // Function to fetch jobs
+  const fetchJobs = async () => {
+    setJobs(jobsData);
+  }
+  
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
 
   const value = {
     searchFilter,
     setSearchFilter,
-    isSearched,setIsSearched,
+    isSearched,
+    setIsSearched,
+    jobs,
+    setJobs,
   };
 
   return (
